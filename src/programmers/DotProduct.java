cd20d1c27080e41cc7975fe9b49f387f81b0cf51
hsLee <gntmddl4261@gmail.com>
2021-01-10 19:25:43 +0900

    프로그래머스 - 내적 구하기

---
+++ src/programmers/DotProduct.java
@@ -0,0 +1,17 @@
+package programmers;
+
+public class DotProduct {
+    public static void main(String[] args) {
+
+    }
+
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+
+        for(int i = 0 ; i < a.length; i++){
+            answer += (a[i] * b[i]);
+        }
+
+        return answer;
+    }
+}
