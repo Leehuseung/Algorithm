0653584c90dc9778d250d0ee2127e24382d98685
hsLee <gntmddl4261@gmail.com>
2021-01-10 19:29:24 +0900

    프로그래머스 - 서울에서 김서방 찾기

---
+++ src/programmers/FindKim.java
@@ -0,0 +1,15 @@
+package programmers;
+
+public class FindKim {
+    public static void main(String[] args) {
+
+    }
+    public String solution(String[] seoul) {
+        for (int i = 0; i < seoul.length; i++) {
+            if(seoul[i].equals("Kim")){
+                return "김서방은 "+i+"에 있다";
+            }
+        }
+        return "";
+    }
+}
