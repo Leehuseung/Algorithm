0ada48b7951e6095d0f8c4b576ccd7cead09667d
hsLee <gntmddl4261@gmail.com>
2021-01-10 18:58:09 +0900

    프로그래머스 - 두정수 사이의합 - 가운데글자 가져오기

---
+++ src/programmers/CenterNumGet.java
@@ -0,0 +1,25 @@
+package programmers;
+
+public class CenterNumGet {
+    public static void main(String[] args) {
+        System.out.println("결과는 : "+solution("abcde"));
+
+    }
+
+    public static String solution(String s) {
+        String answer = "";
+
+        int size = s.length();
+
+        int num = size/2;
+
+        //짝수이면
+        if(size%2 == 0){
+            answer = s.charAt(num-1)+"" +s.charAt(num);
+        //홀수이면
+        } else {
+            answer = s.charAt(num)+"";
+        }
+        return answer;
+    }
+}
---
+++ src/programmers/TwoNumberSum.java
@@ -0,0 +1,26 @@
+package programmers;
+
+public class TwoNumberSum {
+
+    public static void main(String[] args) {
+        System.out.println(solution(3,5));
+        System.out.println(solution(3,3));
+        System.out.println(solution(5,3));
+    }
+
+    public static long solution(int a, int b) {
+        long answer = 0;
+
+        if ( a > b ){
+            int k = a;
+            a = b;
+            b = k;
+        }
+
+        for (int i = a; i <= b; i++) {
+            answer += i;
+        }
+
+        return answer;
+    }
+}
