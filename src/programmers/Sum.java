8fe13ec7469f21d45b71ca1f56fd3fd772b90595
hsLee <gntmddl4261@gmail.com>
2021-01-12 00:28:57 +0900

    프로그래머스 - 자릿수 더하기

---
+++ src/programmers/Sum.java
@@ -0,0 +1,21 @@
+package programmers;
+
+public class Sum {
+    public static void main(String[] args) {
+
+    }
+    public static int solution(int n) {
+        int answer = 0;
+
+        String num = n+"";
+
+        String[] a = num.split("");
+
+        for(int i = 0 ; i < a.length; i++){
+            answer += Integer.parseInt(a[i]);
+        }
+
+
+        return answer;
+    }
+}
