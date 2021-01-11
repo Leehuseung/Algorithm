24328799440e20f8dccd8efd8b7ea4d86925c0d9
hsLee <gntmddl4261@gmail.com>
2021-01-12 00:57:12 +0900

    프로그래머스 -콜라츠 추측

---
+++ src/programmers/Colache.java
@@ -0,0 +1,33 @@
+package programmers;
+
+public class Colache {
+    public static void main(String[] args) {
+        System.out.println(solution(16));
+    }
+    public static int solution(int num) {
+        int answer = 0;
+        long a  = (long)num;
+        if(a == 1) {
+            return 0;
+        }
+
+        while(true){
+            if(answer == 500){
+                return -1;
+            }
+            //짝수이면
+            if(a%2 == 0){
+                a = a/2;
+            //홀수이면
+            } else {
+                a = a * 3 + 1;
+            }
+
+            answer++;
+
+            if(a == 1){
+                return answer;
+            }
+        }
+    }
+}
