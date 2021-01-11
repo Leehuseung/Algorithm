af63c11ca68ece5afb98be9f3b1afc839c9935e6
hsLee <gntmddl4261@gmail.com>
2021-01-12 00:10:52 +0900

    프로그래머스 - 문자열 내림차순 배치하기

---
+++ src/programmers/StringBatch.java
@@ -0,0 +1,22 @@
+package programmers;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class StringBatch {
+    public static void main(String[] args) {
+        System.out.println(solution("nrKesM"));
+    }
+    public static String solution(String s) {
+        String answer = "";
+        String[] arr = s.split("");
+
+        Arrays.sort(arr);
+
+        for (int i = arr.length-1; i >= 0 ; i--) {
+            answer += arr[i];
+        }
+
+        return answer;
+    }
+}
