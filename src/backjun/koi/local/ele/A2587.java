33a25a198827c77eb4370767e42fe551d28bfe78
huseung <gntmd4261@naver.com>
2021-01-24 11:20:49 +0900

    백준 - KOI 지역본선 2004 줄 세우기

---
+++ src/backjun/koi/local/ele/A2587.java
@@ -0,0 +1,28 @@
+package backjun.koi.local.ele;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class A2587 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[5];
+
+        int total = 0;
+
+        for (int i = 0; i < 5; i++) {
+            int num = sc.nextInt();
+            arr[i] = num;
+            total += num;
+        }
+        int average = total/5;
+
+        Arrays.sort(arr);
+
+
+        System.out.println(average);
+        System.out.println(arr[2]);
+    }
+}
