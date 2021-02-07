87ba8e20a7d7e7248ee10f590b5f3f971c1da1fc
hsLee <gntmddl4261@gmail.com>
2021-02-07 14:29:04 +0900

    백준 - 걷기

---
+++ src/backjun/greedy/A1459.java
@@ -0,0 +1,28 @@
+package backjun.greedy;
+
+import java.util.Scanner;
+
+public class A1459 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = Integer.parseInt(sc.nextLine());
+
+        int a = 1000;
+
+        int count = 0;
+
+        num = a - num;
+
+        int[] arr = new int[]{500,100,50,10,5,1};
+
+        for (int i = 0; i < arr.length; i++) {
+            if(num >= arr[i]){
+                int an = num / arr[i];
+                count += an;
+                num -= an * arr[i];
+            }
+        }
+        System.out.println(count);
+    }
+}
