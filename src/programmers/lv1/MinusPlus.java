b42905a549b500fb0bd6ec3c570091c95342bd23
Leehuseung <gntmd4261@naver.com>
2022-01-26 19:37:45 +0900

    프로그래머스 음양 더하기

---
+++ src/programmers/lv1/MinusPlus.java
@@ -0,0 +1,15 @@
+package programmers.lv1;
+
+public class MinusPlus {
+
+    public static int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+
+        for (int i = 0; i < absolutes.length; i++) {
+            answer += (signs[i] ? absolutes[i] : -absolutes[i] );
+        }
+
+        return answer;
+    }
+
+}
