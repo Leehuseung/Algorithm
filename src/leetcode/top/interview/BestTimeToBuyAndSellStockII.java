eb76a5a6e8d407b023d4b856e04605fd266f7ac0
Leehuseung <gntmd4261@naver.com>
2023-07-24 22:27:47 +0900

    LeetCode Best Time to Buy and Sell Stock II (Top Interview)

---
+++ src/leetcode/top/interview/BestTimeToBuyAndSellStockII.java
@@ -0,0 +1,23 @@
+package leetcode.top.interview;
+
+
+/**
+ * 122. Best Time to Buy and Sell Stock II
+ */
+public class BestTimeToBuyAndSellStockII {
+    public static int maxProfit(int[] prices) {
+        int result = 0;
+        for (int i = 0; i < prices.length-1; i++) {
+            if(prices[i+1] - prices[i] > 0) {
+                result += prices[i+1] - prices[i];
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{7,1,5,3,6,4}));
+        System.out.println(maxProfit(new int[]{1,2,3,4,5}));
+        System.out.println(maxProfit(new int[]{5,1,7,2,1}));
+    }
+}
