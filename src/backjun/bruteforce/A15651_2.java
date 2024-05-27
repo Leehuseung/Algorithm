6f4a24be9431c1ccc78c43e816387bcdb6915916
Leehuseung <gntmd4261@naver.com>
2024-05-27 21:45:11 +0900

    백준 N과 M (3)

---
+++ src/backjun/bruteforce/A15651_2.java
@@ -0,0 +1,39 @@
+package backjun.bruteforce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * N과 M (3)
+ */
+public class A15651_2 {
+    static int n;
+    static int m;
+    static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        dfs(1, 0);
+    }
+
+    private static void dfs(int now, int depth) {
+        if(depth == m){
+            for (int i = 0; i < m; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            arr[depth] = i;
+            dfs(i, depth + 1);
+        }
+    }
+}
+
